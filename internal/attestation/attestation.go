21b6130bc195ddfcc3ad7ed0820e5c1a050bb964
Alice Frosi <afrosi@redhat.com>
2026-08-11 09:45:18 +0200

    Add the attestation struct to 3.7 experimental
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- internal/attestation/attestation.go
+++ internal/attestation/attestation.go
@@ -31,7 +31,7 @@ import (
 	"time"
 
 	"github.com/coreos/ignition/v2/config/util"
-	"github.com/coreos/ignition/v2/config/v3_6_experimental/types"
+	"github.com/coreos/ignition/v2/config/v3_7_experimental/types"
 	"github.com/coreos/ignition/v2/internal/log"
 	"github.com/coreos/ignition/v2/internal/resource"
 )
